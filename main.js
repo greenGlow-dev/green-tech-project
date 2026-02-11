6d30543e320e8ebb024a92e4ca0b15908457d233
Your Name <you@example.com>
2026-02-11 16:34:31 +0500

    hoz jonatildi 11.02.2026

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let baza = [
     { img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbjh3VyKm2s062XIHoBcOLBE3YJABlFd4vbw&s`, name: "Abdullo", email: "abdullo@gmail.com" },
     { img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbjh3VyKm2s062XIHoBcOLBE3YJABlFd4vbw&s`, name: "Abdullo", email: "abdullo@gmail.com" },
     { img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbjh3VyKm2s062XIHoBcOLBE3YJABlFd4vbw&s`, name: "Abdullo", email: "abdullo@gmail.com" },
-    { img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbjh3VyKm2s062XIHoBcOLBE3YJABlFd4vbw&s`, name: "Abdullo", email: "abdullo@gmail.com" }, 
+    { img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbjh3VyKm2s062XIHoBcOLBE3YJABlFd4vbw&s`, name: "Abdullo", email: "abdullo@gmail.com" },
     { img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbjh3VyKm2s062XIHoBcOLBE3YJABlFd4vbw&s`, name: "Abdullo", email: "abdullo@gmail.com" },
     { img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbjh3VyKm2s062XIHoBcOLBE3YJABlFd4vbw&s`, name: "Abdullo", email: "abdullo@gmail.com" },
     { img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbjh3VyKm2s062XIHoBcOLBE3YJABlFd4vbw&s`, name: "Abdullo", email: "abdullo@gmail.com" },
@@ -20,38 +20,38 @@ let baza = [
     { img: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbjh3VyKm2s062XIHoBcOLBE3YJABlFd4vbw&s`, name: "Abdullo", email: "abdullo@gmail.com" },
 
 ]
-function newAdd() {
-    for (let i = 0; i < baza.length; i++) {
-        let card = document.createElement("div");
-        let head_card = document.createElement("div");
-        let img = document.createElement("img");
-        let heading = document.createElement("h2");
-        let p = document.createElement("p");
+// function newAdd() {
+//     for (let i = 0; i < baza.length; i++) {
+//         let card = document.createElement("div");
+//         let head_card = document.createElement("div");
+//         let img = document.createElement("img");
+//         let heading = document.createElement("h2");
+//         let p = document.createElement("p");
 
-        img.src = baza[i].img;
-        heading.innerHTML = baza[i].name;
-        p.innerHTML = baza[i].email;
+//         img.src = baza[i].img;
+//         heading.innerHTML = baza[i].name;
+//         p.innerHTML = baza[i].email;
 
-        card.appendChild(img);
-        head_card.appendChild(heading);
-        head_card.appendChild(p);
-        card.appendChild(head_card);
-        card.classList.add("card");
-        head_card.classList.add("head_card");
+//         card.appendChild(img);
+//         head_card.appendChild(heading);
+//         head_card.appendChild(p);
+//         card.appendChild(head_card);
+//         card.classList.add("card");
+//         head_card.classList.add("head_card");
 
-        // head_card.style.border = "1px solid red"
-        // card.style.border = "1px solid red";
-        // card.style.margin = "10px";
+//         // head_card.style.border = "1px solid red"
+//         // card.style.border = "1px solid red";
+//         // card.style.margin = "10px";
 
-        wrap.appendChild(card);
-        console.log("ishladi");
-    }
-}
-
-
+//         wrap.appendChild(card);
+//         console.log("ishladi");
+//     }
+// }
 
 
-newAdd();
+
+
+// newAdd();    
 // let id = document.getElementById("site_bar_id");
 // let id = document.getElementById("header")
 // function loadComponent(id, file) {
@@ -83,5 +83,5 @@ function loadComponent(elementId, filePath) {
 
 // Chaqirish (global o'zgaruvchilarsiz)
 loadComponent("header", "./components/header.html");
-loadComponent("sidebar", "./components/sidebar.html");
-loadComponent("footer", "./components/footer.html");
+// loadComponent("sidebar", "./components/sidebar.html");
+// loadComponent("footer", "./components/footer.html");
